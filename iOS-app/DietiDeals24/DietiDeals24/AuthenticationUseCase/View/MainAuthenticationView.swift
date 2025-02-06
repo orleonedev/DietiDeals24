11b4ea8f6d0472da04b9aa27b7028b8c76c75fc6
Oreste Leone <92297799+orleonedev@users.noreply.github.com>
2025-02-06 16:37:10 +0100

    .

---
+++ iOS-app/DietiDeals24/DietiDeals24/AuthenticationUseCase/View/MainAuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  DietiDeals24
+//
+//  Created by Oreste Leone on 2/6/25.
+//
+
+import SwiftUI
+
+struct MainAuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainAuthenticationView()
+}

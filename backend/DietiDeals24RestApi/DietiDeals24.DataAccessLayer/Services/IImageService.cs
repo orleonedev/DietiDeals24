68fd346513af1c94207b11f1cda33ff45f991aef
Giuseppe Falso <giuseppeefalso@gmail.com>
2025-03-08 19:50:18 +0100

    Upload image with aws s3 now it's on

--- backend/DietiDeals24RestApi/DietiDeals24.DataAccessLayer/Services/IImageService.cs
+++ backend/DietiDeals24RestApi/DietiDeals24.DataAccessLayer/Services/IImageService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using DietiDeals24.DataAccessLayer.Entities;
 using DietiDeals24.DataAccessLayer.Models;
@@ -11,4 +12,5 @@ public interface IImageService
 {
     public Task<Dictionary<Guid, List<string>>> GetImagesUrlsForAuctionAsync(List<Guid> auctionIds);
     public Task<List<string>> GetImagesUrlsForAuctionAsync(Guid auctionId);
+    public Task<Dictionary<Guid, string>> AddImagesUrlsForAuctionAsync(Guid auctionId, List<Guid> imagesId);
 }
